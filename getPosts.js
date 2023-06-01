f40ddc6619b624767b8f7b05907809345bb5e2ba
amami1625 <amami_gt0403@yahoo.co.jp>
2023-06-01 20:55:33 +0900

    カテゴライズする関数を追加

--- getPosts.js
+++ getPosts.js
@@ -5,25 +5,38 @@ const characters = 40; // 投稿のタイトルの文字数の表示上限
 window.addEventListener('DOMContentLoaded', () => {
   fetch(jsonPath)
   .then(res => res.json())
-  .then(resJson => generate_article(resJson));
+  .then(posts => generate_article(posts));
 });
 
-const generate_article = (resJson) => {
-  const postData = resJson.posts.flatMap(post => post.data);
-  const newsData = postData.filter(data => data.category === 'news').slice(0, postsPerPage);
-  const topicsData = postData.filter(data => data.category === 'topics').slice(0, postsPerPage);
+
+/* ====================================================
+ * データをカテゴリ毎に分類する
+ * ==================================================== */
+const categorize_posts = (posts, category) => {
+  const postData = posts.posts.flatMap(post => post.data);
+  const categorized = postData.filter(data => data.category === category).slice(0, postsPerPage);
+  for (const data of categorized) {
+    if (data.title.length >= characters) {
+      data.title = `${data.title.substring(0, characters)}...`;
+    }
+  }
+  return categorized;
+};
+
+/* ====================================================
+ * データから表示するHTMLを作成する
+ * ==================================================== */
+const generate_article = (posts) => {
+  const newsData = categorize_posts(posts, 'news');
+  const topicsData = categorize_posts(posts, 'topics');
   let newsHTML = '';
   let topicsHTML = '';
 
   // news
-  for (const data of newsData) {
-    if (data['title'].length >= characters) {
-      data['title'] = `${data['title'].substring(0, characters)}...`;
-    }
-    
+  for (const data of newsData) {    
     newsHTML = `
     <div class="p-post-list_item">
-      <a href="${data['link']}">
+      <a href="${data.link}">
         <div class="p-post-thumb">
           <figure>
             <img src="asets/image/post_thumb.png">
@@ -31,9 +44,9 @@ const generate_article = (resJson) => {
         </div>
         <div class="p-post-body">
           <div>
-            <time class="p-post-time" datetime="${data['date']}">${data['date']}</time>
+            <time class="p-post-time" datetime="${data.date}">${data.date}</time>
           </div>
-          <h2 class="is-title-style_reset p-post-title">${data['title']}</h2>
+          <h2 class="is-title-style_reset p-post-title">${data.title}</h2>
         </div>
       </a>
     </div>
@@ -43,23 +56,19 @@ const generate_article = (resJson) => {
 
   // topics
   for (const data of topicsData) {
-    if (data['title'].length >= characters) {
-      data['title'] = `${data['title'].substring(0, characters)}...`;
-    }
-
     topicsHTML = `
     <div class="p-post-list_item">
-      <a href="${data['link']}">
+      <a href="${data.link}">
         <div class="p-post-thumb">
           <figure>
-            <img src="${data['thumb']}">
+            <img src="${data.thumb}">
           </figure>
         </div>
         <div class="p-post-body">
           <div>
-            <time class="p-post-time" datetime="${data['date']}">${data['date']}</time>
+            <time class="p-post-time" datetime="${data.date}">${data.date}</time>
           </div>
-          <h2 class="is-title-style_reset p-post-title">${data['title']}</h2>
+          <h2 class="is-title-style_reset p-post-title">${data.title}</h2>
         </div>
       </a>
     </div>
